a4f51d12ca88de420bf7ef0fb010afe3a34348fd
Francis Chong <francis@ignition.hk>
2013-08-15 20:01:31 +0800

    Initial Commit

---
+++ IGDeferred/IGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  IGAppDelegate.h
+//  IGDeferred
+//
+//  Created by Chong Francis on 13年8月15日.
+//  Copyright (c) 2013年 Ignition Soft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ IGDeferred/IGViewController.h
@@ -0,0 +1,13 @@
+//
+//  IGViewController.h
+//  IGDeferred
+//
+//  Created by Chong Francis on 13年8月15日.
+//  Copyright (c) 2013年 Ignition Soft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IGViewController : UIViewController
+
+@end
